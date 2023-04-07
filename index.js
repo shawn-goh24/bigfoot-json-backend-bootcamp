94b7a3c349b62aa8084f3ca4d1a6f60ce75d226c
shawn-goh24 <ahshawngoh@gmail.com>
2023-04-07 18:35:26 +0800

    initial commit till comfortable

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.get("/sightings", async (req, res) => {
+app.use(cors());
+
+app.get("/", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings", async (req, res) => {
+  // console.log(req.query);
+  const sightings = await getSightings();
+
+  // filter by year
+  if (Object.keys(req.query).length !== 0) {
+    const filteredSightings = sightings.filter((sight) => {
+      return sight.YEAR === req.query.year;
+    });
+    res.json(filteredSightings);
+  } else {
+    res.json(sightings);
+  }
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  // console.log(sightings[req.params.sightingIndex]);
+  res.json(sightings[req.params.sightingIndex]);
 });
 
 app.listen(PORT, () => {
